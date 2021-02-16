e9580168787f7ef5175b3cbbb030e3cc46d63e91
TS-N <2501@holbertonschool.com>
2021-02-16 08:18:24 +0000

    pep8 compliant

--- console.py
+++ console.py
@@ -6,108 +6,123 @@ from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-	intro = ''
-	prompt = '(hbnb) '
-	classes = ["BaseModel", "User"]
+    """ HBnB Console
+        Allow to interact with the data storage in an easy way
+        Works in interactive and non-interactive mode
+    """
+    intro = ''
+    prompt = '(hbnb) '
+    classes = ["BaseModel", "User"]
 
+    # ----- Basic HBNB commands -----
+    def do_create(self, arg):
+        """ Creates a new instance of BaseModel, saves it (to the JSON file)
+                and prints the id.
+            Ex: $ create BaseModel
+        """
+        if arg == "":
+            print("** class name missing **")
+        elif arg not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            n = BaseModel()
+            n.save()
+            print(n.id)
 
-	# ----- Basic HBNB commands -----
-	def do_create(self, arg):
-		""" Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id. Ex: $ create BaseModel """
-		if arg == "":
-			print("** class name missing **")
-		elif arg not in self.classes:
-			print("** class doesn't exist **")
-		else:
-			n = BaseModel()
-			n.save()
-			print(n.id)
+    def do_show(self, arg):
+        """ Prints the string representation of an instance
+                based on the class name and id.
+            Ex: $ show BaseModel 1234-1234-1234
+        """
+        a = parse(arg)
+        if a == []:
+            print("** class name missing **")
+        elif a[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(a) == 1:
+            print("** instance id missing **")
+        else:
+            s = "{}.{}".format(a[0], a[1])
+            if s not in models.storage.all():
+                print("** no instance found **")
+            else:
+                print(models.storage.all()[s])
 
-	def do_show(self, arg):
-		""" Prints the string representation of an instance based on the class name and id. Ex: $ show BaseModel 1234-1234-1234 """
-		a = parse(arg)
-		if a == []:
-			print("** class name missing **")
-		elif a[0] not in self.classes:
-			print("** class doesn't exist **")
-		elif len(a) == 1:
-			print("** instance id missing **")
-		else:
-			s = "{}.{}".format(a[0], a[1])
-			if s not in models.storage.all():
-				print ("** no instance found **")
-			else:
-				print(models.storage.all()[s])
+    def do_destroy(self, arg):
+        """  Deletes an instance based on the class name and id
+            (save the change into the JSON file).
+        """
+        a = parse(arg)
+        if a == []:
+            print("** class name missing **")
+        elif a[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(a) == 1:
+            print("** instance id missing **")
+        else:
+            s = "{}.{}".format(a[0], a[1])
+            if s not in models.storage.all():
+                print("** no instance found **")
+            else:
+                del models.storage.all()[s]
+                models.storage.save()
 
-	def do_destroy(self, arg):
-		"""  Deletes an instance based on the class name and id (save the change into the JSON file). """
-		a = parse(arg)
-		if a == []:
-			print("** class name missing **")
-		elif a[0] not in self.classes:
-			print("** class doesn't exist **")
-		elif len(a) == 1:
-			print("** instance id missing **")
-		else:
-			s = "{}.{}".format(a[0], a[1])
-			if s not in models.storage.all():
-				print ("** no instance found **")
-			else:
-				del models.storage.all()[s]
-				models.storage.save()
+    def do_all(self, arg):
+        """ Prints all string representation of all instances
+                based or not on the class name.
+            Ex: $ all BaseModel or $ all.
+        """
+        r = []
+        if arg:
+            if arg not in self.classes:
+                print("** class doesn't exist **")
+                return
+            else:
+                print(models.storage.all())
+                for key, value in models.storage.all().items():
+                    if arg in key:
+                        r.append(value.__str__())
+        else:
+            for key, value in models.storage.all().items():
+                r.append(value.__str__())
+        print(r)
 
-	def do_all(self, arg):
-		""" Prints all string representation of all instances based or not on the class name. Ex: $ all BaseModel or $ all. """
-		r = []
-		if arg:
-			if arg not in self.classes:
-				print("** class doesn't exist **")
-				return
-			else:
-				print(models.storage.all())
-				for key, value in models.storage.all().items():
-					if arg in key:
-						r.append(value.__str__())
-		else:
-			for key, value in models.storage.all().items():
-				r.append(value.__str__())
-		print(r)
+    def do_update(self, arg):
+        """ Updates an instance based on the class name
+                and id by adding or updating attribute
+                (save the change into the JSON file).
+        Ex: $ update BaseModel 1234-1234-1234 email "aibnb@holbertonschool.com"
+        """
+        a = parse(arg)
+        if a == []:
+            print("** class name missing **")
+        elif a[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(a) == 1:
+            print("** instance id missing **")
+        else:
+            s = "{}.{}".format(a[0], a[1])
+            if s not in models.storage.all():
+                print("** no instance found **")
+            elif len(a) == 2:
+                print("** attribute name missing **")
+            elif len(a) == 3:
+                print("** value missing **")
+            else:
+                ob = models.storage.all()[s]
+                setattr(ob, a[2],  a[3])
+                ob.save
 
-	def do_update(self, arg):
-		""" Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file). Ex: $ update BaseModel 1234-1234-1234 email "aibnb@holbertonschool.com". """
-		a = parse(arg)
-		if a == []:
-			print("** class name missing **")
-		elif a[0] not in self.classes:
-			print("** class doesn't exist **")
-		elif len(a) == 1:
-			print("** instance id missing **")
-		else:
-			s = "{}.{}".format(a[0], a[1])
-			if s not in models.storage.all():
-				print ("** no instance found **")
-			elif len(a) == 2:
-				print("** attribute name missing **")
-			elif len(a) == 3:
-				print("** value missing **")
-			else:
-				ob = models.storage.all()[s]
-				setattr(ob, a[2],  a[3])
-				ob.save
+    # ------
 
+    def do_quit(self, arg):
+        """ Quit command to exit the program """
+        return True
 
+    def do_EOF(self, arg):
+        """ Quit command to exit the program """
+        return True
 
-
-
-	# ------
-
-	def do_quit(self, arg):
-		""" Quit command to exit the program """
-		return True
-
-	def do_EOF(self, arg):
-		""" Quit command to exit the program """
-		return True
 
 def parse(arg):
     """ Convert a series of zero or more space separated strings to a list """
--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,49 @@
 #!/usr/bin/python3
-"""Base model class that defines all common attributes/methods for other classes """
-import datetime
+"""
+Base model class that defines all common attributes/methods for other classes
+"""
+from datetime import datetime
 import uuid
 import models
 
-class BaseModel:
 
+class BaseModel:
+    """ BaseModel for HBnB
+        Can interact with the storage
+        Can be called from console
+    """
     def __init__(self, *args, **kwargs):
         if kwargs != {}:
-            for key in kwargs:
-                if key != "__class__":
-                    if key != "created_at" and key != "updated_at":
-                        setattr(self, key, kwargs[key])
+            for k in kwargs:
+                if k != "__class__":
+                    if k != "created_at" and k != "updated_at":
+                        setattr(self, k, kwargs[k])
                     else:
-                        setattr(self, key, datetime.datetime.strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f"))
+                        dt = "%Y-%m-%dT%H:%M:%S.%f"
+                        setattr(self, k, datetime.strptime(kwargs[k], dt))
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
-
     def __str__(self):
-         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id, self.__dict__)
 
     def save(self):
         """ updates the public instance attribute and saves state """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """ generate a dictionary representation of the instance """
         d = {}
         for key, value in self.__dict__.items():
-            if isinstance(value, datetime.datetime):
+            if isinstance(value, datetime):
                 d[key] = value.isoformat()
             else:
                 d[key] = str(value)
         d["__class__"] = self.__class__.__name__
         return d
-
